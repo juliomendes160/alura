8b5109bee612445dfe3a119a74de55f71a775c6c
Julio Mendes <juliomendes160@hotmail.com>
2024-08-03 12:25:02 -0300

    3.2 Montando o layout do formulário

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -20,7 +20,40 @@ class FormularioTransferencia extends StatelessWidget {
       appBar: AppBar(
         title: Text('Criando Tranferência'),
       ),
-      body: Text('Teste'),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+              decoration: InputDecoration(
+                labelText: 'Número da conta',
+                hintText: '0000',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+              decoration: InputDecoration(
+                icon: Icon(Icons.monetization_on),
+                labelText: 'Valor',
+                hintText: '0.00',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          RaisedButton(
+            child: Text('Confirmar'),
+          ),
+        ],
+      ),
     );
   }
 }
